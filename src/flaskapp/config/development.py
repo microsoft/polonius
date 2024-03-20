47762ce9c8b097998dde5d876dc9812ba7b32d31
skraymo1 <kyleraymond@microsoft.com>
2024-03-20 08:51:51 -0500

    more stupidity

--- src/flaskapp/config/development.py
+++ src/flaskapp/config/development.py
@@ -1,7 +1,9 @@
+"""Module configuring development environment."""
+
 import os
 
-from dotenv import load_dotenv
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
