42240a51e82cc2e02af8623dabd6ddcfaf80c093
skraymo1 <kyleraymond@microsoft.com>
2024-03-20 08:57:07 -0500

    add Ruff extension to fix linting

--- src/flaskapp/config/development.py
+++ src/flaskapp/config/development.py
@@ -1,9 +1,9 @@
 """Module configuring development environment."""
 
 import os
+from pathlib import Path
 
 from dotenv import load_dotenv
-from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
--- src/flaskapp/helpers.py
+++ src/flaskapp/helpers.py
@@ -3,8 +3,8 @@ import logging
 import os
 
 import semantic_kernel as sk
-
 from semantic_kernel.connectors.ai.open_ai import AzureChatCompletion
+
 
 class KernelFactory:
     @staticmethod
