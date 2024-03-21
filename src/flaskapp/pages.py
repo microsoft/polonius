2909157b3cb4374a239c704c2bc3d76bad62b0b0
skraymo1 <kyleraymond@microsoft.com>
2024-03-21 09:33:48 -0500

    updated prompt

--- src/flaskapp/pages.py
+++ src/flaskapp/pages.py
@@ -19,7 +19,7 @@ def notes():
 
     # create a UUID for the correlation_id
     correlation_id = helpers.generate_uuid()
-
+    
     input_data = models.TriageNote(
         mrn=data.get("MRN"), stat=data.get("STAT"), age=data.get("Age"), sex=data.get("Sex"), 
         triage=data.get("Triage"), correlation_id=correlation_id
@@ -29,7 +29,8 @@ def notes():
     kernel = helpers.KernelFactory.create_kernel()
 
     running_text_sk_function = kernel.plugins["TriagePlugin"]["Notes"]
-    running_text_args = KernelArguments(input=str(data.get("Triage")))
+    running_text_args = KernelArguments(
+        input=f'{data.get("Triage")} STAT={data.get("STAT")} Age={data.get("Age")} Sex={data.get("Sex")}')
 
     # Flask does not natively support async functions, so we need to create a new event loop
     loop = asyncio.new_event_loop()
