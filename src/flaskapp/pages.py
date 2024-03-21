0cba55cd575cbf3b81007539f291c3fa58783814
skraymo1 <kyleraymond@microsoft.com>
2024-03-21 10:33:24 -0500

    added prompt and testing

--- src/flaskapp/pages.py
+++ src/flaskapp/pages.py
@@ -1,4 +1,5 @@
 import asyncio
+import csv
 
 from flask import Blueprint, render_template, request
 from semantic_kernel.functions.kernel_arguments import KernelArguments
@@ -12,6 +13,9 @@ bp = Blueprint("pages", __name__)
 def index():
     return render_template("index.html")
 
+@bp.get("/api/demo")
+def demo():
+    return "Hello, World!"
 
 @bp.post("/api/notes")
 def notes():
@@ -44,3 +48,70 @@ def notes():
         return str(e)
     finally:
         loop.close()
+
+
+@bp.get("/api/test/notes")
+def notes_test():
+
+    data = {}
+    with open('flaskapp/data/testpages.csv') as file:
+        csv_reader = csv.reader(file)
+        for row in csv_reader:
+            data[row[0]] ={
+                "MRN": row[1],
+                "STAT": row[2],
+                "Age": row[3],
+                "Sex": row[4],
+                "Triage": row[5],
+                "Page" : row[6],
+                "ISS": row[7]
+            }
+        
+    #remove the header
+    items = list(data.items())
+    del items[0]
+    data = dict(items)
+    
+    kernel = helpers.KernelFactory.create_kernel()
+
+    running_text_sk_function = kernel.plugins["TriagePlugin"]["Notes"]
+    updated_data = {}
+
+    first_amount = {k: data[k] for k in list(data)[:10]}
+  
+    for key, value in first_amount.items():
+        running_text_args = KernelArguments(
+            input=f'{value.get("Triage")} STAT={value.get("STAT")} Age={value.get("Age")} Sex={value.get("Sex")}')
+
+        # Flask does not natively support async functions, so we need to create a new event loop
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        try:
+            result = loop.run_until_complete(kernel.invoke(running_text_sk_function, running_text_args))
+            
+            updated_data[key] = {
+                "MRN": value.get("MRN"),
+                "STAT": value.get("STAT"),
+                "Age": value.get("Age"),
+                "Sex": value.get("Sex"),
+                "Triage": value.get("Triage"),
+                "ISS": value.get("ISS"),
+                "Page" : value.get("Page"),
+                "AIPage": str(result)
+            } 
+        except Exception as e:
+            return str(e)
+        finally:
+            loop.close()
+        
+
+    with open('flaskapp/data/results.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        # Write the header
+        writer.writerow(["MRN", "STAT", "Age", "Sex", "Triage", "ISS", "Page", "AIPage"])
+        for key, value in updated_data.items():
+            writer.writerow([value.get("MRN"), value.get("STAT"), value.get("Age"), value.get("Sex"), 
+                             value.get("Triage"), value.get("ISS"), value.get("Page"), value.get("AIPage")])
+       
+
+    return "Done"
